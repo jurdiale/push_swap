756f517cc5883c46c7783d0b49c27a91dddf7ffa
jurdiale <jurdiale@student.42malaga.com>
2026-08-10 19:27:59 +0200

    Parse_flags

--- parse/parse_flags.c
+++ parse/parse_flags.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_flags.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jurdiale <jurdiale@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jurdiale <jurdiale@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 12:08:48 by jurdiale          #+#    #+#             */
-/*   Updated: 2026/08/05 16:21:18 by jurdiale         ###   ########.fr       */
+/*   Updated: 2026/08/10 19:27:21 by jurdiale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ int     parsear_flags(char **argv, int *flag_strategy, int *flag_bench)
     while (argv[i][0] == '-' && argv[i][1] == '-')
     {
         if (ft_strcmp(argv[i], "--simple") == 0)
-            flag_strategy = SIMPLE;
-        if (ft_strcmp(argv[i], "--medium") == 0)
-            flag_strategy = MEDIUM;
-        if (ft_strcmp(argv[i], '--complex') == 0)
-            flag_strategy = COMPLEX;
-        if (ft_strcmp(argv[i], '--adaptive') == 0)
-            flag_strategy = ADAPTATIVE;
-        if (ft_strcmp(argv[i], '--bench') == 0)
-            flag_bench = 1;
+            *flag_strategy = SIMPLE;
+        else if (ft_strcmp(argv[i], "--medium") == 0)
+            *flag_strategy = MEDIUM;
+        else if (ft_strcmp(argv[i], '--complex') == 0)
+            *flag_strategy = COMPLEX;
+        else if (ft_strcmp(argv[i], '--adaptive') == 0)
+            *flag_strategy = ADAPTATIVE;
+        else if (ft_strcmp(argv[i], '--bench') == 0)
+            *flag_bench = 1;
         else
             i = -1;
         i++;
