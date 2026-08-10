3a187bbcd3007d27e2caa879297c432d871ce569
jurdiale <jurdiale@student.42malaga.com>
2026-08-10 21:39:15 +0200

    bonus

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,23 +6,50 @@
 /*   By: jurdiale <jurdiale@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 17:33:21 by jurdiale          #+#    #+#             */
-/*   Updated: 2026/08/10 21:29:10 by jurdiale         ###   ########.fr       */
+/*   Updated: 2026/08/10 21:38:21 by jurdiale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	free_numbers(char **numbers)
+static void	execute_operation(t_stack **a, t_stack **b,
+	char *line, t_count *count)
 {
-	int	j;
+	if (ft_strcmp(line, "sa\n") == 0)
+		sa(a, 0, count);
+	else if (ft_strcmp(line, "sb\n") == 0)
+		sb(b, 0, count);
+	else if (ft_strcmp(line, "ss\n") == 0)
+		ss(a, b, 0, count);
+	else if (ft_strcmp(line, "pa\n") == 0)
+		pa(a, b, 0, count);
+	else if (ft_strcmp(line, "pb\n") == 0)
+		pb(a, b, 0, count);
+	else if (ft_strcmp(line, "ra\n") == 0)
+		ra(a, 0, count);
+	else if (ft_strcmp(line, "rb\n") == 0)
+		rb(b, 0, count);
+	else if (ft_strcmp(line, "rr\n") == 0)
+		rr(a, b, 0, count);
+	else if (ft_strcmp(line, "rra\n") == 0)
+		rra(a, 0, count);
+	else if (ft_strcmp(line, "rrb\n") == 0)
+		rrb(b, 0, count);
+	else if (ft_strcmp(line, "rrr\n") == 0)
+		rrr(a, b, 0, count);
+	else
+		error_exit(a, b);
+}
 
-	j = 0;
-	while (numbers[j] != NULL)
+static int	is_order(t_stack *a)
+{
+	while (a->next != NULL)
 	{
-		free(numbers[j]);
-		j++;
+		if (a->index > a->next->index)
+			return (0);
+		a = a->next;
 	}
-	free(numbers);
+	return (1);
 }
 
 static void	read_and_execute(t_stack **a, t_stack **b, t_count *count)
@@ -36,12 +63,6 @@ static void	read_and_execute(t_stack **a, t_stack **b, t_count *count)
 		free(linea);
 		linea = get_next_line(0);
 	}
-}
-
-static void	free_all(t_stack **a, t_stack **b)
-{
-	free_stack(a);
-	free_stack(b);
 }
 
 int	main(int argc, char **argv)
--- bonus/checker_utils.c
+++ bonus/checker_utils.c
@@ -6,50 +6,29 @@
 /*   By: jurdiale <jurdiale@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 20:37:39 by jurdiale          #+#    #+#             */
-/*   Updated: 2026/08/10 21:29:52 by jurdiale         ###   ########.fr       */
+/*   Updated: 2026/08/10 21:38:24 by jurdiale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_order(t_stack *a)
+void	free_numbers(char **numbers)
 {
-	while (a->next != NULL)
+	int	j;
+
+	j = 0;
+	while (numbers[j] != NULL)
 	{
-		if (a->index > a->next->index)
-			return (0);
-		a = a->next;
+		free(numbers[j]);
+		j++;
 	}
-	return (1);
+	free(numbers);
 }
 
-void	execute_operation(t_stack **a, t_stack **b,
-	char *line, t_count *count)
+void	free_all(t_stack **a, t_stack **b)
 {
-	if (ft_strcmp(line, "sa\n") == 0)
-		sa(a, 0, count);
-	else if (ft_strcmp(line, "sb\n") == 0)
-		sb(b, 0, count);
-	else if (ft_strcmp(line, "ss\n") == 0)
-		ss(a, b, 0, count);
-	else if (ft_strcmp(line, "pa\n") == 0)
-		pa(a, b, 0, count);
-	else if (ft_strcmp(line, "pb\n") == 0)
-		pb(a, b, 0, count);
-	else if (ft_strcmp(line, "ra\n") == 0)
-		ra(a, 0, count);
-	else if (ft_strcmp(line, "rb\n") == 0)
-		rb(b, 0, count);
-	else if (ft_strcmp(line, "rr\n") == 0)
-		rr(a, b, 0, count);
-	else if (ft_strcmp(line, "rra\n") == 0)
-		rra(a, 0, count);
-	else if (ft_strcmp(line, "rrb\n") == 0)
-		rrb(b, 0, count);
-	else if (ft_strcmp(line, "rrr\n") == 0)
-		rrr(a, b, 0, count);
-	else
-		error_exit(a, b);
+	free_stack(a);
+	free_stack(b);
 }
 
 void	build_stack(t_stack **a, t_stack **b, char **numbers)
