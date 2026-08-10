b6bf2536424c0c03e3ab3a9fd788ded9277987e0
Pablo Fajardo Mayor <pfajardo@VictusPFajardo.localdomain>
2026-08-10 22:42:27 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabfajar <pabfajar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 22:35:45 by pabfajar          #+#    #+#             */
+/*   Updated: 2026/08/10 22:41:50 by pabfajar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	init_all(t_stack **a, t_stack **b, t_count *count)
+{
+	*a = NULL;
+	*b = NULL;
+
+	ft_memset(count, 0, sizeof(t_count));
+}
+
+int	parse_and_validate(int argc, char **argv, t_stack **a, t_stack **b)
+{
+	int		i;
+	int		j;
+	char	**numbers;
+
+	i = parse_flags(argv);
+	if (argc == i + 1)
+		numbers = ft_split(argv[i], ' ');
+	else
+		numbers = argv + i;
+	j = 0;
+	while (numbers[j])
+	{
+		if (!is_valid(numbers[j]) || !push_stack(a, ft_atoi(numbers[j])))
+			error_exit(&a, &b);
+		j++;
+	}
+	if (there_are_duplicates(*a))
+		error_exit(&a, &b);
+	normalize(*a);
+	return (1);
+}
+
+void	execute_algorithms(t_stack **a, t_stack **b, t_count *count)
+{
+	int		size;
+	float	disorder;
+	int		flag_strategy;
+
+	disorder = compute_disorder(*a);
+	size = stack_size(*a);
+	flag_strategy = ADAPTATIVE;
+	if (size == 2)
+		order_2(a, count);
+	else if (size == 3)
+		order_3(a, count);
+	else if (size > 3)
+	{
+		if (flag_strategy == SIMPLE)
+			simple_algorithm(a, b, count);
+		else if (flag_strategy == MEDIUM)
+			medium_algorithm(a, b, count);
+		else if (flag_strategy == COMPLEX)
+			complex_algorithm(a, b, count);
+		else
+			adaptative(a, b, disorder, count);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+	t_count	*count;
+	int		flag_bench;
+	float	disorder;
+
+	flag_bench = 0;
+	disorder = compute_disorder(a);
+
+	if (argc == 1)
+		return (0);
+	init_all(&a, &b, &count);
+	if (!parse_and_validate(argc, argv, &a, &b))
+		error_exit(&a, &b);
+	execute_algorithms(&a, &b, &count);
+	if (flag_bench)
+		bench(disorder, &count, flag_bench);
+	free_stack(&a);
+	free_stack(&b);
+	return (0);
+}
